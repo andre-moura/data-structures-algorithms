432887df1a0e57ed4b05fe7fc2cede66182eb2d3
andre-moura <61800450+andre-moura@users.noreply.github.com>
2022-12-11 16:00:57 -0300

    append method and construtor for doubly linked list

--- 04-Doubly Linked List/doubly_linked_list.py
+++ 04-Doubly Linked List/doubly_linked_list.py
@@ -1,2 +1,24 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
 class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = self.tail = new_node
+        self.length = 1
+
     
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+
