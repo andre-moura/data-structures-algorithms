740d0dc0e35541dbc5547297a26da5b840095563
andre-moura <61800450+andre-moura@users.noreply.github.com>
2022-12-11 10:51:55 -0300

    adding remove method to linked list

--- 02-Classes/linked_list.py
+++ 02-Classes/linked_list.py
@@ -82,7 +82,7 @@ class LinkedList:
 
     def insert(self, index, value):
         if index < 0 or index > self.length:
-            return True
+            return False
         
         if index == 0:
             return self.prepend(value)
@@ -95,6 +95,22 @@ class LinkedList:
         temp.next = new_node
         self.length += 1
         return True
+
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+
+        prev = self.get(index - 1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
 
 
     def print_list(self):
