dfd035c5e9fe2b06a5862e8b7e17e360468f0d7b
andre-moura <61800450+andre-moura@users.noreply.github.com>
2022-12-20 08:31:16 -0300

    Adding stack class and modifying doubly linked list

--- 04-Doubly Linked List/doubly_linked_list.py
+++ 04-Doubly Linked List/doubly_linked_list.py
@@ -29,7 +29,6 @@ class DoublyLinkedList:
         temp = self.tail
         if self.length == 1:
             self.tail = self.head = None
-            return 
         else:
             self.tail = self.tail.prev
             self.tail.next = None
@@ -37,3 +36,83 @@ class DoublyLinkedList:
         self.length -= 1
         return temp
         
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.length += 1
+        return True
+
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        if self.length == 1:
+            self.head = self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+            temp.next = None
+        self.length -= 1
+        return temp
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        if index < self.length / 2:
+            for _ in range(index):
+                temp = temp.next
+        else:
+            temp = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev
+        return temp
+    
+
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+    
+
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        
+        new_node = Node(value)
+        before = self.get(index - 1)
+        after = before.next
+
+        new_node.prev = before
+        before.next = new_node
+        new_node.next = after
+        after.prev = new_node
+        self.length += 1
+        return True
+        
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        
+        temp = self.get(index)
+        temp.next.prev = temp.prev
+        temp.prev.next = temp.next
+        temp.next = temp.prev = None
+        self.length -= 1
+        return temp
---
+++ 05-Stack/stack.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next  = None
+
+
+class Stack:
+    def __init__(self, value):
+        self.node = Node(value)
+        self.top = self.node
+        self.height = 1
+
+    
+    def push(self, value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.height += 1
+        return True
