35dbb1a95e32150585be698bcb551182902f9277
andre-moura <61800450+andre-moura@users.noreply.github.com>
2022-12-23 09:31:07 -0300

    Adding pop method to the stack

--- 05-Stack/stack.py
+++ 05-Stack/stack.py
@@ -20,3 +20,12 @@ class Stack:
             self.top = new_node
         self.height += 1
         return True
+
+    def pop(self):
+        if self.height == 0:
+            return None
+        temp = self.top
+        self.top = self.top.next
+        temp.next = None
+        self.height -= 1
+        return temp
