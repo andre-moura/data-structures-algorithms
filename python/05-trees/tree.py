c9588f0bd4c79ab0a8768b5dc8723958dac12bc0
andre-moura <61800450+andre-moura@users.noreply.github.com>
2022-12-27 08:39:00 -0300

    Adding insert tree

--- python/05-trees/tree.py
+++ python/05-trees/tree.py
@@ -6,6 +6,27 @@ class Node:
 
 
 class BinarySearchTree:
-    def __init__(self, value):
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
         new_node = Node(value)
-        self.root = new_node
+        if self.root is None: 
+            self.root = new_node
+            return True
+
+        temp = self.root
+        while temp is not None:
+            if new_node.value == temp.value:
+                return False
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp.right
+
